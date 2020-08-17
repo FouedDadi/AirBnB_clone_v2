f43d94200ec626476edbd44b4e4866fbe41d490c
FouedDadi <foued.dadii@gmail.com>
2020-08-17 18:18:05 +0100

    prepare servers for deployement

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 # script that sets up web servers for the deployment of web_static
 sudo apt-get update
-sudo apt-get -y upgrade
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 echo "keep quiet plz" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '45i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
