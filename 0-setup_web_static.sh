c391e67924f281e93463499251681f709c949699
FouedDadi <foued.dadii@gmail.com>
2020-08-17 17:39:18 +0100

    prepare servers for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,5 +6,5 @@ sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 sudo echo "keep quiet plz" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu: /data/
-sudo sed -i "\\tlocation \/hbnb_static/ {\n\t\t alias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
+sudo sed -i "\tlocation \/hbnb_static/ {\n\t alias /data/web_static/current/;\n}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
