63eae7784cde5d51e76fb603e4a127447ff1289c
FouedDadi <foued.dadii@gmail.com>
2020-09-02 21:32:56 +0100

    start a flask web application aand load all cities of a state

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application:
+    To load all cities of a State:
+    If your storage engine is DBStorage, you must use cities relationship
+    Otherwise, use the public getter method cities
+    After each request you must remove the current SQLAlchemy Session:
+    Declare a method to handle @app.teardown_appcontext
+    Call in this method storage.close()
+    Routes:
+    /cities_by_states: display a HTML page: (inside the tag BODY)
+    H1 tag: “States”
+    UL tag: with the list of all State objects present in DBStorage sorted
+    by name (A->Z) tip
+    LI tag: description of one State: <state.id>: <B><state.name></B> + UL tag:
+    with the list
+    of City objects linked to the State sorted by name (A->Z)
+    LI tag: description of one City: <city.id>: <B><city.name></B>
+"""
+
+from models.state import State
+from models import storage
+from flask import Flask, escape, request
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def ct_list():
+    sts = storage.all(State).values()
+    return render_template("8-cities_by_states.html", sts=sts)
+
+
+@app.teardown_appcontext
+def clos(self):
+    storage.close()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
